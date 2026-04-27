650b01a55fb78a2ce79e7aa366d6ca7cba92a8da
LIMONCITO045 <juliancin04@gmail.com>
2026-04-27 13:25:46 -0600

    csv corregido

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-df = pd.read_csv('d3_aire01_49_1.csv')
+df = pd.read_csv('d3_aire01_49_1_limpio.csv')
 df.info()
 
 #df_estados_
